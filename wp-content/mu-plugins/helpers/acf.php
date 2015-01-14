7e7613f53e461c6095042211b83470de3a374107
Joseph Furlott <jmfurlott@gmail.com>
2015-01-14 13:41:24 -0500

    added helpers && home page acf

---
+++ wp-content/mu-plugins/helpers/acf.php
@@ -0,0 +1,59 @@
+<?php
+
+// Home page
+
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_home-page',
+		'title' => 'Home Page',
+		'fields' => array (
+			array (
+				'key' => 'field_54b6b76d2c768',
+				'label' => 'First Impression Title',
+				'name' => 'first_impression_title',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_54b6b7752c769',
+				'label' => 'First Impression Subtitle',
+				'name' => 'first_impression_subtitle',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page',
+					'operator' => '==',
+					'value' => '8',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+
+
+?>
