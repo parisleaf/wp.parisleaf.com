c17cc6d6cd41da564900c7bca14fff8ac25f6e96
Andrew Clark <acdlite@me.com>
2015-01-19 09:52:42 -0500

    Add tagline field to projects

--- wp-content/mu-plugins/helpers/acf.php
+++ wp-content/mu-plugins/helpers/acf.php
@@ -1,7 +1,5 @@
 <?php
 
-// Home page
-
 function get_id_by_slug($wpdb, $slug) {
   $id = $wpdb->get_var("SELECT ID FROM $wpdb->posts WHERE post_name = '".$slug."'");
   return $id;
@@ -9,6 +7,9 @@ function get_id_by_slug($wpdb, $slug) {
 
 if(function_exists("register_field_group"))
 {
+  /**
+   * Home page
+   */
 	register_field_group(array (
 		'id' => 'acf_home-page',
 		'title' => 'Home Page',
@@ -44,6 +45,47 @@ if(function_exists("register_field_group"))
 					'param' => 'page',
 					'operator' => '==',
 					'value' => get_id_by_slug($wpdb, 'home'),
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+
+  /**
+   * Projects
+   */
+  register_field_group(array (
+		'id' => 'acf_projects',
+		'title' => 'Projects',
+		'fields' => array (
+			array (
+				'key' => 'field_54bd19358a2d9',
+				'label' => 'Tagline',
+				'name' => 'tagline',
+				'type' => 'text',
+				'instructions' => 'Brief tagline or teaser describing the project.',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'project',
 					'order_no' => 0,
 					'group_no' => 0,
 				),
