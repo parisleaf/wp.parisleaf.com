732a64cb8b8d91510487dbb03f38b10b5de3f36e
Joseph Furlott <jmfurlott@gmail.com>
2015-01-26 13:29:53 -0500

    Added acf-repeater to load

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -5,6 +5,7 @@ require_once('json-rest-api/plugin.php');
 require_once('Basic-Auth/basic-auth.php');
 require_once('wp-api-menus/plugin.php');
 require_once('advanced-custom-fields/acf.php');
+require_once('acf-repeater/acf-repeater.php');
 require_once('WP-JSON-API-ACF/plugin.php');
 
 require_once('parisleaf/index.php');
