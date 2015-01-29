ef2d99fe491682878423ecaa159fac53a2e01649
Joseph Furlott <jmfurlott@gmail.com>
2015-01-29 11:31:55 -0500

    fixed load to mu

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -7,6 +7,7 @@ require_once('wp-api-menus/plugin.php');
 require_once('advanced-custom-fields/acf.php');
 require_once('acf-repeater/acf-repeater.php');
 require_once('acf-options-page/acf-options-page.php');
+require_once('wp-api-acf-options/wp-api-acf-options.php');
 require_once('WP-JSON-API-ACF/plugin.php');
 
 require_once('parisleaf/index.php');
