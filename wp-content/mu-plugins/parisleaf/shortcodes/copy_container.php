c02a1b6ed0d6b7abf0e49225abcd3948bbed636e
Joseph Furlott <jmfurlott@gmail.com>
2015-02-02 16:42:08 -0500

    Updated copy_container to allow image using img parameter

--- wp-content/mu-plugins/parisleaf/shortcodes/copy_container.php
+++ wp-content/mu-plugins/parisleaf/shortcodes/copy_container.php
@@ -1,42 +1,53 @@
 <?php
-
 use Mexitek\PHPColors\Color;
-
 function pl_copy_container( $atts, $content = null ) {
 	$a = shortcode_atts( array(
       'background_color' => null,
       'text_color' => null,
   ), $atts );
-
 	if (is_null($a['text_color']) && !is_null($a['background_color'])) {
 		$background_color = new Color($a['background_color']);
 		$a['text_color'] = $background_color->isDark() ? '#e2eaf2' : '#343844';
 	}
-
 	$style = [];
 	$classes = ['CopyContainer'];
-
-	if (in_array('primary', $atts)) $classes[] = 'CopyContainer--primary';
-	if (in_array('secondary', $atts)) $classes[] = 'CopyContainer--secondary';
-
+	if (in_array('primary', $a)) $classes[] = 'CopyContainer--primary';
+	if (in_array('secondary', $a)) $classes[] = 'CopyContainer--secondary';
 	if ($a['background_color']) {
 		$style[] = 'background-color: '.$a['background_color'].';';
 	}
-
 	if ($a['text_color']) {
 		$style[] = 'color: '.$a['text_color'].';';
 	}
-
 	// Begin shortcode output
 	ob_start();
 ?>
-<div class="<?php echo implode($classes, ' ') ?>" <?php echo count($style) ? 'style="'.implode($style).'"' : ''?>>
-	<div class="SiteContainer">
-		<?php echo $content; ?>
-	</div>
+<div class="<?php echo implode($classes, ' '); ?>" <?php echo count($style) ? 'style="'.implode($style).'"' : ''?>>
+<?php
+    if((is_array($atts))) {
+      if((array_key_exists('img', $atts))) { //no image
+?>
+        <div class="copy_container">
+          <div class="copy_container-content">
+            <?php echo $content; ?>
+          </div>
+          <div class='copy_container-img'>
+            <img src="<?php echo $atts['img'] ?>">
+          </div>
+        </div>
+<?php
+       } 
+     } else { //there is not an image, so site container 
+?>
+	  <div class="SiteContainer">
+            <?php echo $content; ?>
+ 	  </div>
+<?php
+      } 
+    
+?>
 </div>
 <?php
 	return ob_get_clean();
 }
-
 add_shortcode( 'copy_container', 'pl_copy_container' );
