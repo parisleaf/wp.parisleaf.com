1e8f86cce9941a6e49b3e10e200c848de8152480
Joseph Furlott <jmfurlott@gmail.com>
2015-02-05 14:58:12 -0500

    added wp-api-yoast

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -9,5 +9,6 @@ require_once('acf-repeater/acf-repeater.php');
 require_once('acf-options-page/acf-options-page.php');
 require_once('wp-api-acf-options/wp-api-acf-options.php');
 require_once('WP-JSON-API-ACF/plugin.php');
+require_once('wp-api-yoast/plugin.php');
 
 require_once('parisleaf/index.php');
