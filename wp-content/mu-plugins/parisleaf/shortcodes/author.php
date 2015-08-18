eeeec471ff3bae52d0bfc27b319b58d6c18be4e2
Eric Leite <eric@parisleaf.com>
2015-08-17 21:22:43 -0400

    added missing echo to gravatar

--- wp-content/mu-plugins/parisleaf/shortcodes/author.php
+++ wp-content/mu-plugins/parisleaf/shortcodes/author.php
@@ -14,7 +14,7 @@ function pl_author($atts) {
 ?>
 
 <footer class='Author'>
-  <?php get_avatar( get_the_author_meta( 'ID' ), 384); ?>
+  <?php echo get_avatar( get_the_author_meta( 'ID' ), 384); ?>
   <h5>About <?php the_author(); ?></h5>
   <p class="Author-content"><?php the_author_meta( 'description' ); ?></p>
 </footer>
