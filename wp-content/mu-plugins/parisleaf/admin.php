8b91e065d7f774e6e582f242cb1353dc14055d55
Eric Leite <eric@parisleaf.com>
2015-10-06 14:38:02 -0400

    Emptied admin.php file

--- wp-content/mu-plugins/parisleaf/admin.php
+++ wp-content/mu-plugins/parisleaf/admin.php
@@ -1,11 +1,1 @@
 <?php
-
-/**
- * Customize admin login screen
- */
-function pl_set_admin_logo() {
-  echo '<style type="text/css">
-    h1 a { background-image:url("https://parisleaf.com/img/admin-logo.png") !important; }
-  </style>';
-}
-add_action( 'login_head', 'pl_set_admin_logo' );
