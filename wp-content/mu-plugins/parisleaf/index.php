3ab7a4b779c82761c5ef154c24f3c843d0b9e399
Eric Leite <eric@parisleaf.com>
2015-10-06 14:12:36 -0400

    Attempt to find media upload bug

--- wp-content/mu-plugins/parisleaf/index.php
+++ wp-content/mu-plugins/parisleaf/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('admin.php');
-require_once('acf.php');
-require_once('custom-post-types/project.php');
-require_once('taxonomies/project_tag.php');
-require_once('shortcodes/shortcodes.php');
+// require_once('admin.php');
+// require_once('acf.php');
+// require_once('custom-post-types/project.php');
+// require_once('taxonomies/project_tag.php');
+// require_once('shortcodes/shortcodes.php');
 
 /**
  * Force set permalink structure on admin page load
