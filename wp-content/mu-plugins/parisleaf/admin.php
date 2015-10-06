4cac6804ad746ecce1aea87250793817f69c645c
Eric Leite <eric@parisleaf.com>
2015-10-06 14:34:01 -0400

    Removed login logo title

--- wp-content/mu-plugins/parisleaf/admin.php
+++ wp-content/mu-plugins/parisleaf/admin.php
@@ -14,16 +14,3 @@ function pl_set_admin_logo_url() {
   return get_bloginfo( 'url' );
 }
 add_filter( 'login_headerurl', 'pl_set_admin_logo_url' );
-
-function pl_set_admin_logo_url_title() {
-  return 'Parisleaf';
-}
-add_filter( 'login_headertitle', 'pl_set_admin_logo_url_title' );
-
-/**
- * Add editor styles
- */
-// function pl_add_editor_styles() {
-//   add_editor_style( 'custom-editor-style.css' );
-// }
-// add_action( 'admin_init', 'pl_add_editor_styles' );
