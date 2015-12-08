c9dd012136e4331dd17cd64a5a2d59dc1cb70b8a
Eric Leite <eric@parisleaf.com>
2015-12-07 20:31:07 -0500

    Added copy section shortcode

---
+++ wp-content/mu-plugins/parisleaf/shortcodes/copy_section.php
@@ -0,0 +1,45 @@
+<?php
+// SHORTCODE: Copy Section
+// input: [section modifiers]content[/section]
+// output:
+// <div class="CopySection">
+//   <div class="CopySection-inner">
+//     $content
+//   </div>
+//  </div>
+
+function pl_is_flag( $flag, $atts ) {
+  foreach ( $atts as $key => $value )
+    if ( $value === $flag && is_int( $key ) ) return true;
+  return false;
+}
+
+function pl_copy_section( $atts, $content = null ) {
+	// Clean atts
+  $clean_atts = array_map('sanitize_text_field', $atts);
+
+	// Set section classes
+	$classes = ['CopySection'];
+	if ( pl_is_flag( 'full', $clean_atts ) ) {
+		$classes[] = 'CopySection--full';
+	} else {
+		$classes[] = 'CopySection--contain';
+	}
+
+	// Begin shortcode output
+	ob_start();
+
+?>
+
+<div class="<?php echo implode($classes, ' '); ?>">
+  <div class="CopySection-inner">
+    <?php echo do_shortcode($content); ?>
+  </div>
+</div>
+
+<?php
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'section', 'pl_copy_section' );
